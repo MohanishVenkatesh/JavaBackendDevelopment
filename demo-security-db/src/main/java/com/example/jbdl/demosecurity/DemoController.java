f168dae86fcad296fb5753a81e63605d394e433b
=MohanishVenkatesh <=>
2022-03-05 13:40:19 +0400

    demo-security-db project added

---
+++ demo-security-db/src/main/java/com/example/jbdl/demosecurity/DemoController.java
@@ -0,0 +1,48 @@
+package com.example.jbdl.demosecurity;
+
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+
+    //This can be accessed by anyone without logging in
+    @GetMapping("/hello")
+    public  String sayHello(){
+        return "Hello Guest!!!";
+    }
+    // This can be accessed  by only those people who have STUDENT authority
+    @GetMapping("/student/hello")
+    public String sayHelloToStudent()
+    {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        User user = (User)authentication.getPrincipal();
+        return "Hello user!!" + user.getUsername();
+    }
+    // This can be accessed by only those people who have Admin authority
+    @GetMapping("/admin/hello")
+    public String sayHellotoAdmin()
+    {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        User user = (User)authentication.getPrincipal();
+        return "Hello admin!!" + user.getUsername();
+    }
+    //Both student and admin should be able to access this API
+    @GetMapping("/student/attendance")
+    public Integer getAttendance()
+    {
+        return 0 ;
+    }
+    //Only the student should be able to access the API
+    @PostMapping("/student/attendance")
+    public String submitAttendance()
+    {
+        return null ;
+    }
+
+}
